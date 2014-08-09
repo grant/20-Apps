bb0703324287bc916c30ea5431a51568da0167e6
Grant Timmerman <granttimmerman@gmail.com>
2014-08-09 10:17:37 -0700

    Add hello world app

---
+++ Day 1 Hello World/Day 1 Hello World/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Day 1 Hello World
+//
+//  Created by Grant Timmerman on 8/9/14.
+//  Copyright (c) 2014 Grant Timmerman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
