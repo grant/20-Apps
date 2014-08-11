c75ba3c5e7fd9f6d530370f564ed296e77090913
Grant Timmerman <granttimmerman@gmail.com>
2014-08-11 04:26:00 -0700

    Add basic gui for day 3

--- Day 3 Alert/Day 3 Alert/ViewController.h
+++ Day 3 Alert/Day 3 Alert/ViewController.h
@@ -10,6 +10,7 @@
 
 @interface ViewController : UIViewController
 
+@property UIButton* launchButton;
 
 @end
 
--- Day 3 Alert/Day 3 Alert/ViewController.m
+++ Day 3 Alert/Day 3 Alert/ViewController.m
@@ -18,6 +18,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
+    // Background
+    CGFloat r = 221;
+    CGFloat g = 86;
+    CGFloat b = 70;
+    UIColor* bgColor = [UIColor colorWithRed:(r/255) green:(g/255) blue:(b/255) alpha:255];
+    self.view.backgroundColor = bgColor;
+    
+    // Launch button
+    _launchButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    _launchButton.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height);
+    _launchButton.titleLabel.font = [UIFont systemFontOfSize:20];
+    [_launchButton setTitle:@"Launch the missles!" forState:UIControlStateNormal];
+    [_launchButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [_launchButton setExclusiveTouch:YES];
+    [self.view addSubview:_launchButton];
 }
 
 - (void)didReceiveMemoryWarning {
