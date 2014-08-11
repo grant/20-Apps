9b1ebe7239832991d8c17a9ba329c19e224f7cb5
Grant Timmerman <granttimmerman@gmail.com>
2014-08-11 03:59:46 -0700

    Init day 3

---
+++ Day 3 Alert/Day 3 Alert/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Day 3 Alert
+//
+//  Created by Grant Timmerman on 8/11/14.
+//  Copyright (c) 2014 Grant Timmerman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Day 3 Alert/Day 3 Alert/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  Day 3 Alert
+//
+//  Created by Grant Timmerman on 8/11/14.
+//  Copyright (c) 2014 Grant Timmerman. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+            
+
+@end
+
+@implementation ViewController
+            
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
