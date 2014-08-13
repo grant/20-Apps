95d23bda4b3268053c88cf0fd8d48ba9b41b1558
Grant Timmerman <granttimmerman@gmail.com>
2014-08-13 10:41:33 -0700

    Fix cod etypo

--- Day 5 Strobe/Day 5 Strobe/ViewController.m
+++ Day 5 Strobe/Day 5 Strobe/ViewController.m
@@ -24,7 +24,7 @@
     float b = 54;
     float g = 29;
     UIColor *backgroundColor = [UIColor colorWithRed:r/255 green:g/255 blue:b/255 alpha:255];
-    [self.view setBackgroundColor:b ackgroundColor];
+    [self.view setBackgroundColor:backgroundColor];
 }
 
 - (void)didReceiveMemoryWarning {
