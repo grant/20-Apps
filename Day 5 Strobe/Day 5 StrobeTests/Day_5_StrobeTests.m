24f3d6cabb672a0affef306c7fc9e3d372c81f0b
Grant Timmerman <granttimmerman@gmail.com>
2014-08-13 10:32:33 -0700

    Init day 5

---
+++ Day 5 Strobe/Day 5 StrobeTests/Day_5_StrobeTests.m
@@ -0,0 +1,39 @@
+//
+//  Day_5_StrobeTests.m
+//  Day 5 StrobeTests
+//
+//  Created by Grant Timmerman on 8/13/14.
+//  Copyright (c) 2014 Grant Timmerman. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Day_5_StrobeTests : XCTestCase
+
+@end
+
+@implementation Day_5_StrobeTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
