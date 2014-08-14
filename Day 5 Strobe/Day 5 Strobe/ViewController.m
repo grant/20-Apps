55bbae34c399fc05f32926360ee249f688aabf5b
Grant Timmerman <granttimmerman@gmail.com>
2014-08-13 19:48:32 -0700

    Add basic strobe light functionality

--- Day 5 Strobe/Day 5 Strobe/ViewController.m
+++ Day 5 Strobe/Day 5 Strobe/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
 @interface ViewController ()
             
@@ -14,6 +15,10 @@
 @end
 
 @implementation ViewController
+
+float sliderValue = 0.5;
+bool lightAvailable;
+AVCaptureDevice *light;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -28,6 +33,14 @@
     
     // Setup slider
     [_slider addTarget:self action:@selector(sliderValueChanged:) forControlEvents:UIControlEventValueChanged];
+    
+    // Setup strobelight
+    [NSTimer scheduledTimerWithTimeInterval:0.02 target:self selector:@selector(strobeUpdate:) userInfo:nil repeats:YES];
+    lightAvailable = (NSClassFromString(@"AVCaptureDevice") != nil);
+    if (lightAvailable) {
+        light = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+        lightAvailable = ([light hasTorch] && [light hasFlash]);
+    }
 }
 
 - (void)didReceiveMemoryWarning {
@@ -36,7 +49,39 @@
 }
 
 - (IBAction) sliderValueChanged:(UISlider *)sender {
-    NSLog(@"%@", [NSString stringWithFormat:@"%.2f", [sender value]]);
+    sliderValue = [sender value];
+//    NSLog(@"%@", [NSString stringWithFormat:@"%.2f", [sender value]]);
+}
+
+- (void)strobeUpdate:(id)sender {
+    if (lightAvailable) {
+        if ([self lightIsOn]) {
+            [self turnOffLight];
+        } else {
+            [self turnOnLight];
+        }
+    }
+}
+
+// Turns on the camera light
+- (void)turnOnLight {
+    [light lockForConfiguration:nil];
+    [light setTorchMode:AVCaptureTorchModeOn];
+    [light setFlashMode:AVCaptureFlashModeOn];
+    [light unlockForConfiguration];
+}
+
+// Turns off the camera light
+- (void)turnOffLight {
+    [light lockForConfiguration:nil];
+    [light setTorchMode:AVCaptureTorchModeOff];
+    [light setFlashMode:AVCaptureFlashModeOff];
+    [light unlockForConfiguration];
+}
+
+// Returns true if the light is on
+- (bool)lightIsOn {
+    return light.torchMode == AVCaptureTorchModeOn;
 }
 
 @end
