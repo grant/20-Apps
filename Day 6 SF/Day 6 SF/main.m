9495b9a27dd428c046ffbb986565b61128b58987
Grant Timmerman <granttimmerman@gmail.com>
2014-08-14 18:38:46 -0700

    Init day 6

---
+++ Day 6 SF/Day 6 SF/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Day 6 SF
+//
+//  Created by Grant Timmerman on 8/14/14.
+//  Copyright (c) 2014 Grant Timmerman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Day 6 SF/Day 6 SFTests/Day_6_SFTests.m
@@ -0,0 +1,39 @@
+//
+//  Day_6_SFTests.m
+//  Day 6 SFTests
+//
+//  Created by Grant Timmerman on 8/14/14.
+//  Copyright (c) 2014 Grant Timmerman. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Day_6_SFTests : XCTestCase
+
+@end
+
+@implementation Day_6_SFTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
