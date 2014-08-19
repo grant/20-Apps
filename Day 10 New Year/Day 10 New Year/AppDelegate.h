67311264037c2f23569f7e227c6effd0f9295024
Grant Timmerman <granttimmerman@gmail.com>
2014-08-18 22:42:59 -0700

    Init day 10

---
+++ Day 10 New Year/Day 10 New Year/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Day 10 New Year
+//
+//  Created by Grant Timmerman on 8/18/14.
+//  Copyright (c) 2014 Grant Timmerman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Day 10 New Year/Day 10 New Year/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Day 10 New Year
+//
+//  Created by Grant Timmerman on 8/18/14.
+//  Copyright (c) 2014 Grant Timmerman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
