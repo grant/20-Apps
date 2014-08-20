16a8924ed46e0a3e8509f6ee99fc3990eddfc419
Grant Timmerman <granttimmerman@gmail.com>
2014-08-20 14:00:00 +0100

    Created white app

--- Day 11 HN/Day 11 HN/ViewController.m
+++ Day 11 HN/Day 11 HN/ViewController.m
@@ -13,16 +13,37 @@
 
 @end
 
+NSArray *tableData;
+
 @implementation ViewController
             
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    tableData = [NSArray arrayWithObjects:@"Egg Benedict", @"Mushroom Risotto", @"Full Breakfast", @"Hamburger", @"Ham and Egg Sandwich", @"Creme Brelee", @"White Chocolate Donut", @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle with Egg", @"Noodle with BBQ Pork", @"Japanese Noodle with Pork", @"Green Tea", @"Thai Shrimp Cake", @"Angry Birds Cake", @"Ham and Cheese Panini", nil];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [tableData count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"SimpleTableCell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    
+    cell.textLabel.text = [tableData objectAtIndex:indexPath.row];
+    return cell;
 }
 
 @end
--- Day 12 White/Day 12 White/ViewController.m
+++ Day 12 White/Day 12 White/ViewController.m
@@ -13,15 +13,39 @@
 @end
 
 @implementation ViewController
-            
+
+UISwitch *lightSwitch;
+UIColor *backgroundColor;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // Add UISwitch
+    float x = self.view.frame.size.width/2 - 20;
+    float y = self.view.frame.size.height/2;
+    lightSwitch = [[UISwitch alloc] initWithFrame:CGRectMake(x, y, self.view.frame.size.width, 0)];
+    [lightSwitch addTarget:self action:@selector(flip:) forControlEvents:UIControlEventValueChanged];
+    [self.view addSubview:lightSwitch];
+    
+    // Setup background
+    float r = 253;
+    float g = 140;
+    float b = 37;
+    backgroundColor = [UIColor colorWithRed:r/255 green:g/255 blue:b/255 alpha:255];
+    [self.view setBackgroundColor:backgroundColor];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)flip:(id)sender {
+    if (lightSwitch.on) {
+        [self.view setBackgroundColor:[UIColor whiteColor]];
+    } else {
+        [self.view setBackgroundColor:backgroundColor];
+    }
 }
 
 @end
