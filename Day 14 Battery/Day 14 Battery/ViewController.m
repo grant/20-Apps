03c8f43cf90edf7783fa4ef12c9941eb7bb9a283
Grant Timmerman <granttimmerman@gmail.com>
2014-08-22 17:00:00 +0100

    Add basic battery level

--- Day 14 Battery/Day 14 Battery/ViewController.m
+++ Day 14 Battery/Day 14 Battery/ViewController.m
@@ -13,15 +13,35 @@
 @end
 
 @implementation ViewController
-            
+
+UILabel *batteryLabel;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // Setup battery label
+    batteryLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
+    [batteryLabel setTextAlignment:NSTextAlignmentCenter];
+    [self.view addSubview:batteryLabel];
+    [self updateBatteryDisplay];
+    
+    // Setup battery notification system
+    [[UIDevice currentDevice] setBatteryMonitoringEnabled:YES];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(batteryLevelUpdate:) name:UIDeviceBatteryLevelDidChangeNotification object:nil];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)batteryLevelUpdate:(NSNotification *)notification {
+    // do whatever, using: [[UIDevice currentDevice] batteryLevel]
+}
+
+- (void)updateBatteryDisplay {
+    float batteryLevel = [[UIDevice currentDevice] batteryLevel];
+    [batteryLabel setText:[NSString stringWithFormat:@"%.02f", batteryLevel]];
 }
 
 @end
