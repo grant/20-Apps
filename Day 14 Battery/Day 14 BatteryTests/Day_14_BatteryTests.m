dbd96d852d53e1715adf852e913e9ed869b52d0e
Grant Timmerman <granttimmerman@gmail.com>
2014-08-22 17:00:00 +0100

    Init day 14

---
+++ Day 14 Battery/Day 14 BatteryTests/Day_14_BatteryTests.m
@@ -0,0 +1,40 @@
+//
+//  Day_14_BatteryTests.m
+//  Day 14 BatteryTests
+//
+//  Created by Grant Timmerman on 8/24/14.
+//  Copyright (c) 2014 Grant Timmerman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Day_14_BatteryTests : XCTestCase
+
+@end
+
+@implementation Day_14_BatteryTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
