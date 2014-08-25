3d71fcc047c5c55530d198509f960e388555c203
Grant Timmerman <granttimmerman@gmail.com>
2014-08-25 09:08:42 -0700

    Init day 17

---
+++ Day 17 Swipe/Day 17 Swipe/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Day 17 Swipe
+//
+//  Created by Grant Timmerman on 8/25/14.
+//  Copyright (c) 2014 Grant Timmerman. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+            
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Day 17 Swipe/Day 17 Swipe/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Day 17 Swipe
+//
+//  Created by Grant Timmerman on 8/25/14.
+//  Copyright (c) 2014 Grant Timmerman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
