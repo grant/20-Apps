cae302ca30d8ca3b5f5ea75cc0417effc2dc98f2
Grant Timmerman <granttimmerman@gmail.com>
2014-08-25 22:51:12 -0700

    Add swipe recognizers

--- Day 17 Swipe/Day 17 Swipe/ViewController.m
+++ Day 17 Swipe/Day 17 Swipe/ViewController.m
@@ -27,11 +27,38 @@
     label.textColor = [UIColor whiteColor];
     [self.view addSubview:label];
     
+    // Swipe recognizers
+    UISwipeGestureRecognizer *swipeLeft = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipeLeft:)];
+    UISwipeGestureRecognizer *swipeUp = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipeUp:)];
+    UISwipeGestureRecognizer *swipeRight = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipeRight:)];
+    UISwipeGestureRecognizer *swipeDown = [[UISwipeGestureRecognizer alloc] initWithTarget:self action:@selector(handleSwipeDown:)];
+    swipeLeft.direction = UISwipeGestureRecognizerDirectionLeft;
+    swipeUp.direction = UISwipeGestureRecognizerDirectionUp;
+    swipeRight.direction = UISwipeGestureRecognizerDirectionRight;
+    swipeDown.direction = UISwipeGestureRecognizerDirectionDown;
+    [[self view] addGestureRecognizer:swipeLeft];
+    [[self view] addGestureRecognizer:swipeUp];
+    [[self view] addGestureRecognizer:swipeRight];
+    [[self view] addGestureRecognizer:swipeDown];
+    
     // Set background
     CGFloat r = 255;
     CGFloat g = 201;
     CGFloat b = 55;
     self.view.backgroundColor = [UIColor colorWithRed:(r/255) green:(g/255) blue:(b/255) alpha:255];
+}
+
+- (IBAction)handleSwipeLeft:(UISwipeGestureRecognizer*)sender {
+    NSLog(@"left");
+}
+- (IBAction)handleSwipeUp:(UISwipeGestureRecognizer*)sender {
+    NSLog(@"up");
+}
+- (IBAction)handleSwipeRight:(UISwipeGestureRecognizer*)sender {
+    NSLog(@"right");
+}
+- (IBAction)handleSwipeDown:(UISwipeGestureRecognizer*)sender {
+    NSLog(@"down");
 }
 
 - (void)didReceiveMemoryWarning {
