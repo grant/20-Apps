6c15da7d8941f75f0d8b441a8a9c3e4b74fc680c
Grant Timmerman <granttimmerman@gmail.com>
2014-08-27 21:59:27 -0700

    Init day 19

---
+++ Day 19 Camera/Day 19 Camera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Day 19 Camera
+//
+//  Created by Grant Timmerman on 8/27/14.
+//  Copyright (c) 2014 Grant Timmerman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Day 19 Camera/Day 19 CameraTests/Day_19_CameraTests.swift
@@ -0,0 +1,36 @@
+//
+//  Day_19_CameraTests.swift
+//  Day 19 CameraTests
+//
+//  Created by Grant Timmerman on 8/27/14.
+//  Copyright (c) 2014 Grant Timmerman. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Day_19_CameraTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
